e329751268f0ff54074af9f999786234df579afa
marcrine <marcrinemm@gmail.com>
2022-10-14 16:15:19 +0300

    changes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -15,44 +15,40 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>
-    Replaces any underscores in <text> with slashes.
-    """
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Displays C followed by a text variable"""
     text = text.replace("_", " ")
     return "C {}".format(text)
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>
-    Replaces any underscores in <text> with slashes.
-    """
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text = "is cool"):
+    """Displays Python and text variable"""
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """Displays 'n is a number' only if <n> is an integer."""
+    """Displays 'n is a number' only if n is an integer."""
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """Displays an HTML page only if <n> is an integer."""
     return render_template("5-number.html", n=n)
@@ -60,4 +56,3 @@ def number_template(n):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-    
